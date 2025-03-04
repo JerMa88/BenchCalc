2a3bf8e31712ecf6e8cfcd3c3ee64b0fc0e6a5a0
Jerry Ma <jerryma@smu.edu>
2025-03-04 08:52:02 -0600

    Initial Commit

---
+++ BenchCalcTests/BenchCalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  BenchCalcTests.swift
+//  BenchCalcTests
+//
+//  Created by zma on 3/4/25.
+//
+
+import Testing
+@testable import BenchCalc
+
+struct BenchCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
